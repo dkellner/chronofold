f22a412f92248f1d310f7aea1928563875a9f0b3
Dominik Kellner <dkellner@dkellner.de>
2022-02-11 05:56:07 +0100

    Add `flake.{nix,lock}` for Nix Flakes support

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            rust-bin.stable.latest.default
+            cargo-expand
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
